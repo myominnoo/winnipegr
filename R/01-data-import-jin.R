70b79cdcf733eebe947348e9a5a92c8b51937473
ywjin0707 <yw_jin0707@hotmail.com>
2026-03-07 15:04:26 -0600

    [data-import-jin.R] adjusted query and added tree-inventory

--- R/01-data-import-jin.R
+++ R/01-data-import-jin.R
@@ -2,19 +2,20 @@ source("R/00-helpers.R")
 
 # ── API endpoint list ────────────────────────────────────────────────────────── -----
 
-threemonthsback <- (now() - days(60)) |> lubridate::format_ISO8601()
+onemonthback <- (now() - days(30)) |> lubridate::format_ISO8601()
 threeyearsback <- (now() - days(365 * 3)) |> lubridate::format_ISO8601()
 
 api_list <- list(
-  wfps_call_logs              = paste0("https://data.winnipeg.ca/resource/yg42-q284.json?$where=call_time>'", threemonthsback, "'&$group=neighbourhood,incident_type&$select=neighbourhood,incident_type,count(*)"),
+  wfps_call_logs              = paste0("https://data.winnipeg.ca/resource/yg42-q284.json?$where=call_time>'", onemonthback, "'&$group=neighbourhood,incident_type&$select=neighbourhood,incident_type,count(*)"),
   aggregate_building_permits  = paste0("https://data.winnipeg.ca/resource/p5sy-gt7y.json?$where=year>=", year(threeyearsback), "&$group=neighbourhood,permit_group&$select=neighbourhood,permit_group,sum(total_declared_construction_value),sum(total_permits),sum(major_projects_count)"),
   tree_inventory              = "https://data.winnipeg.ca/resource/hfwk-jp4h.json?$group=neighbourhood&$select=neighbourhood,count(*)",
   # mosquito_trap               = "https://data.winnipeg.ca/resource/du7c-8488.json",
-  request311                 = paste0("https://data.winnipeg.ca/resource/u7f6-5326.json?$where=open_date>'", threemonthsback, "'&$group=neighbourhood,type&$select=neighbourhood,type,count(*)")
+  request311                 = paste0("https://data.winnipeg.ca/resource/u7f6-5326.json?$where=open_date>'", onemonthback, "'&$group=neighbourhood,type&$select=neighbourhood,type,count(*)")
 )
 
 wfps_call_logs <- fetch_one_page(api_list$wfps_call_logs)
 aggregate_building_permits <- fetch_one_page(api_list$aggregate_building_permits)
+tree_inventory <- fetch_one_page(api_list$tree_inventory)
 request311 = fetch_one_page(api_list$request311)
 
 # Takes ~3 minutes to run
